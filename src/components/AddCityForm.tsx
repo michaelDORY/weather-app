dd49a52737a33f605e63c4010c25d3c4e7fc7db3
michaelDORY <mish.makwww@gmail.com>
2022-07-17 21:15:28 +0300

    added trim()

--- src/components/AddCityForm.tsx
+++ src/components/AddCityForm.tsx
@@ -11,7 +11,7 @@ const AddCityForm: FC = memo(() => {
 
   useEffect(() => {
     if (city) {
-      dispatch(setShouldBeAdded(city))
+      dispatch(setShouldBeAdded(city.trim()))
     }
   }, [city])
 
